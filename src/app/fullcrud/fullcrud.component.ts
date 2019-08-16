a7b0004c57be5e67898fef1d551c63d36720ff48
Anderson Jimenez <anderson.jimenez@farleyetc.com>
2019-08-16 16:50:27 -0500

    Better Web

--- src/app/fullcrud/fullcrud.component.ts
+++ src/app/fullcrud/fullcrud.component.ts
@@ -28,8 +28,8 @@ imagen5 = '';
         this.descripcion = 'Éste proyecto es sencillo pero interesante, puesto que se consulta un API para los elementos que se ' +
         'muestran en los selectores y una vez que el usuario escoge las opciones, las convierte en variables que serán ' +
         'enviadas de nuevo a otra instancia del API y de esta forma hacer la consulta con las monedas deseadas...';
-        this.linkDemo = 'asd';
-        this.linkGithub = 'asd';
+        this.linkDemo = 'https://naughty-dubinsky-279fc0.netlify.com';
+        this.linkGithub = 'https://github.com/AndersonJz/proyectos/tree/master/criptomonedas';
         this.imagen1 = './assets/img/theme/cripto.png';
         this.imagen2 = '';
         this.imagen3 = '';
@@ -37,12 +37,12 @@ imagen5 = '';
       case 'eventos':
       this.titulo = 'Buscador de Eventos';
       this.descripcionCorta = 'Busca eventos segun los criterios que escoja el usuario.';
-      this.descripcion = 'En este proyecto construido en React JS, el usuario puede escoger una categoría y un criterio como una ciudada ' +
+      this.descripcion = 'En este proyecto construido en React JS, el usuario puede escoger una categoría y un criterio como una ciudad ' +
       'o un tema de interes, esta app consume datos de un API llamada EventBrite, y para este caso se utiliza una libreria ' +
       'llamada Context en la cual se reciben los datos y estos quedas disponibles para cualqier componente que se encierre dentro de ' +
       'este, Context viene a sustituir a los comúnes "props" de React JS...';
-      this.linkDemo = 'asd';
-      this.linkGithub = 'asd';
+      this.linkDemo = 'https://gracious-heyrovsky-d05971.netlify.com/';
+      this.linkGithub = 'https://github.com/AndersonJz/proyectos/tree/master/eventosreact';
       this.imagen1 = './assets/img/theme/eventos1.png';
       this.imagen2 = './assets/img/theme/eventos.png';
       this.imagen3 = './assets/img/theme/eventos2.png';
@@ -60,6 +60,34 @@ imagen5 = '';
       this.imagen3 = './assets/img/theme/remyperfil.png';
       this.imagen4 = './assets/img/theme/remyservicios.png';
       this.imagen5 = './assets/img/theme/remyunservicio.png';
+      break;
+      case 'restaurante':
+      this.titulo = 'App Restaurante';
+      this.descripcionCorta = 'Aplicacion para gestionar los platillos que se sirven en un restaurante';
+      this.descripcion = 'Este proyecto es uno de mis favoritos, pues aunque no tiene muchas funcionalidades, ' +
+      'es una aplicación diseñada en el stack MERN (MongoDB, Express, React JS, Node Js), por lo que en ella te muestro ' +
+      'mis habilidades tanto en Back-end, creando un API rest que se conecta a MongoDB y que responde ' +
+      'datos en formato JSON, asi como Front-end, consumiendo los datos mencionados anteriormente y organizandolos ' +
+      'de una forma amigable para el usuario, asi como el uso de las rutas proporcionadas por el API... ' +
+      'Proximamente con login y gestión de imagenes :)';
+      this.linkDemo = 'https://kind-wozniak-26bab1.netlify.com';
+      this.linkGithub = 'https://github.com/AndersonJz/proyectos/tree/master/mern';
+      this.imagen1 = './assets/img/theme/mern1.png';
+      this.imagen2 = './assets/img/theme/mern2.png';
+      this.imagen3 = './assets/img/theme/mern3.png';
+      break;
+      case 'buscadorletras':
+      this.titulo = 'Buscador de letras de Canciones';
+      this.descripcionCorta = 'Aplicacion para buscar letras de canciones por Artista';
+      this.descripcion = 'Lo interesante de esta aplicación es la utilización de dos APIs simultaneas, ' +
+      'una para buscar la letra de la cancion que el usuario requiera, y ' +
+      'otra que toma información de la consulta para buscar información del artista en cuestión... ' +
+      'Estas APIs trabajan con mucha informacion por lo que puede tardar un par de segundos en responderte';
+      this.linkDemo = 'https://heuristic-poitras-abc5ff.netlify.com';
+      this.linkGithub = 'https://github.com/AndersonJz/proyectos/tree/master/buscadorletras';
+      this.imagen1 = './assets/img/theme/letras.png';
+      this.imagen2 = './assets/img/theme/letras2.png';
+      this.imagen3 = './assets/img/theme/letras3.png';
       break;
 
     }
